935c52bb9e2185e26770125a5f9d2dcd79fd174a
Mannong Pang <pang@carnegiehub.org>
2019-09-09 14:52:38 -0700

    Lecture4/5

---
+++ app.js
@@ -0,0 +1,14 @@
+(function(){
+'use strict';
+
+angular.module('myFirstApp',[])
+
+.controller('MyFirstController', function($scope){
+	$scope.name = "Yaakov";
+	$scope.sayHello = function(){
+		return "Hello Coursera!";
+	}
+
+});
+
+})();
