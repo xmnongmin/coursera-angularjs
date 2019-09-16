6163c15843ce1288065fa0eb74813da9dfe7cb40
Mannong Pang <pang@carnegiehub.org>
2019-09-15 18:36:22 -0700

    Lecture15

--- app.js
+++ app.js
@@ -4,42 +4,34 @@
 angular.module('CounterApp', [])
 .controller('CounterController', CounterController);
 
-CounterController.$inject = ['$scope'];
-
-function CounterController($scope) {
-  $scope.onceCounter = 0;
+CounterController.$inject = ['$scope','$timeout'];
+function CounterController($scope,$timeout) {
   $scope.counter = 0;
-  $scope.name = "Yaakov";
 
-  $scope.showNumberOfWatchers = function () {
-    console.log("# of Wachers: " + JSON.stringify($scope.$$watchersCount));
+  $scope.upCounter = function () {
+    $timeout(function(){
+       $scope.counter++;
+      console.log("Counter incremented!");
+    },2000);  
   };
 
-  $scope.countOnce = function(){
-    $scope.onceCounter = 1;
-  };
+  // $scope.upCounter = function () {
+  //   setTimeout(function () {
+  //     $scope.$apply(function () {
+  //       $scope.counter++;
+  //       console.log("Counter incremented!");
+  //     });
+  //   }, 2000);
+  // };
 
-  $scope.upCounter  = function(){
-    $scope.counter++;
-  }
-
-  // 4th Watcher
-  $scope.$watch(function(){ 
-    console.log("Digest loop fired!");
-  });
-
-  // $scope.$watch('onceCounter', function(newValue, oldValue){
-  //   console.log("old value: " + oldValue);
-  //   console.log("new value: " + newValue);
-  // });
-
-  // $scope.$watch('counter', function(newValue, oldValue){
-  //   console.log("old value: " + oldValue);
-  //   console.log("new value: " + newValue);
-  // });
-
-
-
+  // $scope.upCounter = function () {
+  //   setTimeout(function () {
+  //     $scope.counter++;
+  //     console.log("Counter incremented!");
+  //     $scope.$digest();
+  //   }, 2000);
+  // };
+  
 }
 
 })();
