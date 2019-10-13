73cd52dcd5de65aeaca3ed13bc994576d2697de9
Mannong Pang <pang@carnegiehub.org>
2019-10-13 16:20:13 -0700

    Lecture25

--- app.js
+++ app.js
@@ -1,169 +1,65 @@
 (function () {
 'use strict';
 
-angular.module('ShoppingListPromiseApp', [])
-.controller('ShoppingListController', ShoppingListController)
-.service('ShoppingListService', ShoppingListService)
-.service('WeightLossFilterService', WeightLossFilterService);
-
-ShoppingListController.$inject = ['ShoppingListService'];
-function ShoppingListController(ShoppingListService) {
-  var list = this;
-
-  list.items = ShoppingListService.getItems();
-
-  list.itemName = "";
-  list.itemQuantity = "";
-
-  list.addItem = function () {
-    ShoppingListService.addItem(list.itemName, list.itemQuantity);
-  };
-
-  list.removeItem = function (itemIndex) {
-    ShoppingListService.removeItem(itemIndex);
-  };
-}
+angular.module('MenuCategoriesApp', [])
+.controller('MenuCategoriesController', MenuCategoriesController)
+.service('MenuCategoriesService', MenuCategoriesService)
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
 
 
-ShoppingListService.$inject = ['$q', 'WeightLossFilterService'];
-function ShoppingListService($q, WeightLossFilterService) {
-  var service = this;
+MenuCategoriesController.$inject = ['MenuCategoriesService'];
+function MenuCategoriesController(MenuCategoriesService) {
+  var menu = this;
 
-  // List of shopping items
-  var items = [];
+  var promise = MenuCategoriesService.getMenuCategories();
 
-  //The regular way promise.then(success{}, fails{});
-  // service.addItem = function(name, quantity){
-  //   var promise = WeightLossFilterService.checkName(name);
+  promise.then(function (response) {
+    menu.categories = response.data;
+  })
+  .catch(function (error) {
+    console.log("Something went terribly wrong.");
+  });
 
-  //   promise.then(function(response){
-  //     var nextPromise = WeightLossFilterService.checkQuantity(quantity);
-      
-  //     nextPromise.then(function(response){
-  //       var item = {
-  //         name: name,
-  //         quantity: quantity
-  //       };
-  //         items.push(item);
-  //       }, function(errorResponse){
-  //       console.log(errorResponse.message);      
-  //       });
-  //     },
+  menu.logMenuItems = function (shortName) {
+    var promise = MenuCategoriesService.getMenuForCategory(shortName);
 
-  //     function(errorResponse){
-  //     console.log(errorResponse.message);
-  //   });
-
-  // };
-
-  //Second way to code promise - cleaner
-  // service.addItem = function(name, quantity){
-  //   var promise = WeightLossFilterService.checkName(name);
-
-  //   promise
-  //   .then(function(reponse){
-  //     return WeightLossFilterService.checkQuantity(quantity);
-  //   })
-  //   .then(function(response){
-  //     var item = {
-  //       name: name,
-  //       quantity: quantity
-  //     };
-  //     items.push(item);
-  //   })
-  //   .catch(function(errorResponse){
-  //     console.log(errorResponse.message);
-  //   });
-  // };
-
-  //third way to code promise through $q
-  service.addItem = function(name, quantity){
-    var namePromise = WeightLossFilterService.checkName(name);
-    var quantityPromise = WeightLossFilterService.checkQuantity(quantity);
-
-    $q.all([namePromise, quantityPromise]).
-    then(function(response){
-      var item = {
-        name: name,
-        quantity: quantity
-      };
-      items.push(item);
+    promise.then(function (response) {
+      console.log(response.data);
     })
-    .catch(function(errorResponse){
-      console.log(errorResponse.message);
-    });
-  };
-  
-  // service.addItem = function (name, quantity) {
-  //   var namePromise = WeightLossFilterService.checkName(name);
-  //   var quantityPromise = WeightLossFilterService.checkQuantity(quantity);
-
-  //   $q.all([namePromise, quantityPromise]).
-  //   then(function (response) {
-  //     var item = {
-  //       name: name,
-  //       quantity: quantity
-  //     };
-  //     items.push(item);
-  //   })
-  //   .catch(function (errorResponse) {
-  //     console.log(errorResponse.message);
-  //   });
-  // };
-
-  service.removeItem = function (itemIndex) {
-    items.splice(itemIndex, 1);
+    .catch(function (error) {
+      console.log(error);
+    })
   };
 
-  service.getItems = function () {
-    return items;
-  };
 }
 
 
-WeightLossFilterService.$inject = ['$q', '$timeout'];
-function WeightLossFilterService($q, $timeout) {
+MenuCategoriesService.$inject = ['$http', 'ApiBasePath'];
+function MenuCategoriesService($http, ApiBasePath) {
   var service = this;
 
-  service.checkName = function(name){
-    var deferred = $q.defer();
-    
-    var result = {
-      message: ""
-    };
+  service.getMenuCategories = function () {
+    var response = $http({
+      method: "GET",
+      url: (ApiBasePath + "/categories.json")
+    });
 
-    $timeout(function() {
-      if(name.toLowerCase().indexOf('cookie') === -1){
-        deferred.resolve(result);
-      }
-      else{
-        result.message = "Stay away from cookies, Yaakov!";
-        deferred.reject(result);
-      }
-    }, 1000);
-
-    return deferred.promise;
+    return response;
   };
 
-  service.checkQuantity = function(quantity){
-    var deferred = $q.defer();
-    
-    var result = {
-      message: ""
-    };
 
-    $timeout(function() {
-      if(quantity < 6){
-        deferred.resolve(result);
+  service.getMenuForCategory = function (shortName) {
+    var response = $http({
+      method: "GET",
+      url: (ApiBasePath + "/menu_items.json"),
+      params: {
+        category: shortName
       }
-      else{
-        result.message = "That's too much, Yaakov!";
-        deferred.reject(result);
-      }
-    }, 1000);
+    });
 
-    return deferred.promise;
+    return response;
   };
+
 }
 
 })();
--- prototype.js
+++ prototype.js
@@ -47,3 +47,57 @@
 //
 // // Not being used as a function constructor
 // Dog("Max2");
+
+
+//callback functions
+// first(2, function(firstResult, err){ 
+// 	if(!err){
+// 		second(firstResult,function(secondResult, err){
+// 			if(!err){
+// 				third(secondResult, function(thirdResult, err){
+// 					console.log(thirdResult);
+// 				});
+// 			}
+// 		});
+// 	}
+// });
+// function first(value, fname){
+// 	fname(value+2, false);
+// }
+
+// function second(value, fname){
+// 	fname(value+2, false);
+// }
+
+// function third(value, fname){
+// 	fname(value+2, false);
+// }
+
+//promise
+var promise = new Promise(function(resolve, reject){
+	alert('abc');
+	resolve(2);
+	alert('def');
+});
+
+promise.then(first).then(second).then(third).then(function(response){
+	console.log(response);
+});
+
+function first(value){
+	alert('a');
+	return value+2;
+}
+
+function second(value){
+	alert('b');
+	return value+2;
+}
+
+function third(value){
+	alert('c');
+	return value+2;
+}
+
+
+
